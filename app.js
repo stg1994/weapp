60d8da5ed9069e5a125089e29fbbeeda2a339b88
stg1994 <2815811994@qq.com>
2018-03-20 23:05:11 +0800

    six commit

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const config = require('./config')
 
 
 
+
 // 使用响应处理中间件
 app.use(response)
 
@@ -19,6 +20,9 @@ app.use(bodyParser())
 // 引入路由分发
 const router = require('./routes')
 app.use(router.routes())
+
+
+
 
 // 启动程序，监听端口
 app.listen(config.port, () => debug(`listening on port ${config.port}`))
